6fdbd3273dc870ac2e591fde865a785cf30e54d8
70KG <zhangjianbing777@163.com>
2019-04-15 11:27:46 +0800

    SpringBoot自动装配

---
+++ springboot-autoconfig-chapter/src/main/java/com/zhangjianbing/autoconfig/AutoConfigApplication.java
@@ -0,0 +1,17 @@
+package com.zhangjianbing.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张建兵 Ryan
+ * time 2019/4/15
+ */
+@SpringBootApplication
+public class AutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigApplication.class, args);
+    }
+
+}
