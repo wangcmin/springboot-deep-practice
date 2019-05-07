e97ada67a886a4e7387eb22a91a9f5ab44efc72f
zhangjianbing <449246146@qq.com>
2019-05-07 23:18:34 +0800

    SpringBoot2.0深度实践-添加webflux依赖

--- springboot-web-chapter/src/main/java/com/zhangjianbing/story/SpringbootWebApplication.java
+++ springboot-web-chapter/src/main/java/com/zhangjianbing/story/SpringbootWebApplication.java
@@ -3,6 +3,14 @@ package com.zhangjianbing.story;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * @author Ryan Zhang
@@ -18,6 +26,20 @@ public class SpringbootWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebApplication.class, args);
+    }
+
+    /**
+     * Description: 增加 webflux 函数式 Endpoint代码
+     * @author: Ryan Zhang
+     * Date: 2019/5/7
+     * From: https://www.zhangjianbing.com
+     */
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return route(GET("/hello-world"),
+                request -> ok().body(Mono.just("hello,world"), String.class)
+
+        );
     }
 
 }
