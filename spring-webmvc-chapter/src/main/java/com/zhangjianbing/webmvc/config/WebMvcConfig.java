a9eca11ee5c46a1c248f22048093a99a475ea8b1
zhangjianbing <449246146@qq.com>
2019-06-12 22:39:59 +0800

    SpringBoot2.0 deep practice

---
+++ spring-webmvc-chapter/src/main/java/com/zhangjianbing/webmvc/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.zhangjianbing.webmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author Ryan Zhang
+ * @Title: WebMvcConfig
+ * @Description:
+ * @date 2019/6/11
+ * @From https://www.zhangjianbing.com
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+
+}
